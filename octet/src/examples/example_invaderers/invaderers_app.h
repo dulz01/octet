c7214c814713f8faa5fcc8e8dcfb781ac4d64deb
dulz01 <yuureijin@gmail.com>
2016-10-14 15:41:43 +0100

    Added 2D movement to the ship in invaderers. Ship also collides with top and bottom borders.

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -248,13 +248,30 @@ namespace octet {
     // use the keyboard to move the ship
     void move_ship() {
       const float ship_speed = 0.05f;
-      // left and right arrows
+
+      // arrow keys
       if (is_key_down(key_left)) {
         sprites[ship_sprite].translate(-ship_speed, 0);
         if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+2])) {
           sprites[ship_sprite].translate(+ship_speed, 0);
         }
-      } else if (is_key_down(key_right)) {
+      } 
+      
+      if (is_key_down(key_up)) {
+        sprites[ship_sprite].translate(0, +ship_speed);
+        if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+1])) {
+          sprites[ship_sprite].translate(0, -ship_speed);
+        }
+      }
+
+      if (is_key_down(key_down)) {
+        sprites[ship_sprite].translate(0, -ship_speed);
+        if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+0])) {
+          sprites[ship_sprite].translate(0, +ship_speed);
+        }
+      }
+
+      if (is_key_down(key_right)) {
         sprites[ship_sprite].translate(+ship_speed, 0);
         if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+3])) {
           sprites[ship_sprite].translate(-ship_speed, 0);
