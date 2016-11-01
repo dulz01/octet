62fe0bb775aadd17d6832ae0a06a4b86f2651adc
dulz01 <yuureijin@gmail.com>
2016-11-01 23:59:11 +0000

    Reduced the number of enemies for testing.

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -30,8 +30,8 @@ namespace octet {
 
     enum {
       num_sound_sources = 8,
-      num_rows = 5,
-      num_cols = 10,
+      num_rows = 1,
+      num_cols = 1,
       num_missiles = 2,
       num_bombs = 2,
       num_borders = 4,
