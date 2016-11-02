027393986f48923c2a813b334d6603d39f7cbc4a
dulz01 <yuureijin@gmail.com>
2016-11-02 20:55:27 +0000

    Line endings conversion from reading the code

--- octet/src/examples/example_shapes/example_shapes.h
+++ octet/src/examples/example_shapes/example_shapes.h
@@ -19,14 +19,14 @@ namespace octet {
 
     /// this is called once OpenGL is initialized
     void app_init() {
-      app_scene =  new visual_scene();
+      app_scene = new visual_scene();
       app_scene->create_default_camera_and_lights();
       app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 4, 0));
 
       material *red = new material(vec4(1, 0, 0, 1));
       material *green = new material(vec4(0, 1, 0, 1));
       material *blue = new material(vec4(0, 0, 1, 1));
-	  material *yellow = new material(vec4(1, 1, 0, 1));
+      material *yellow = new material(vec4(1, 1, 0, 1));
 
       mat4t mat;
       mat.translate(-3, 6, 0);
@@ -37,12 +37,12 @@ namespace octet {
       app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), red, true);
 
       mat.loadIdentity();
-      mat.translate( 3, 6, 0);
+      mat.translate(3, 6, 0);
       app_scene->add_shape(mat, new mesh_cylinder(zcylinder(vec3(0, 0, 0), 2, 4)), blue, true);
 
-	  mat.loadIdentity();
-	  mat.translate(0, 0, 0);
-	  app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), yellow, true);
+      mat.loadIdentity();
+      mat.translate(0, 0, 0);
+      app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), yellow, true);
 
       // ground
       mat.loadIdentity();
@@ -57,7 +57,7 @@ namespace octet {
       app_scene->begin_render(vx, vy);
 
       // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+      app_scene->update(1.0f / 30);
 
       // draw the scene
       app_scene->render((float)vx / vy);
